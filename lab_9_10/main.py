1bf8e9d2fc8739bf8fa9c526bab64ce5604792f0
jarrok3 <jarrok333@gmail.com>
2025-05-25 15:34:49 +0200

    model implement, fixes required

--- lab_9_10/main.py
+++ lab_9_10/main.py
@@ -12,14 +12,26 @@ if __name__ == "__main__":
     X_mean = X.mean(axis=0)
     X_std = X.std(axis=0)
     X = (X - X_mean) / X_std
+    
     # Get output classes
     num_classes = len(np.unique(y))
-    y_onehot = np.eye(num_classes)[y]  
+    y_onehot = np.eye(num_classes)[y] # classes vector of quality values
 
     X_train, X_test, y_train, y_test = train_test_split(X, y_onehot, test_size=0.2, random_state=42)
+    # X_val, X_test, y_val, y_test   = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42)
+
+    training_data   = [(x.reshape(-1, 1), y.reshape(-1, 1)) for x, y in zip(X_train, y_train)]
+    # validation_data = [(x.reshape(-1, 1), y.reshape(-1, 1)) for x, y in zip(X_val, y_val)]
+    test_data       = [(x.reshape(-1, 1), y.reshape(-1, 1)) for x, y in zip(X_test, y_test)]
 
     num_features = X_train.shape[1]
     num_classes = y_train.shape[1]
     net = Network([num_features, 16, num_classes]) # 3 layers
+    
+    net.gradient_descent(training_data, max_steps=10000, learning_rate=0.1)
+    correct = net.evaluate(test_data)
+    total   = len(test_data)
+    accuracy = correct / total * 100
+    print(f"Accuracy on test set: {correct}/{total} correct ({accuracy:.2f}%)")
 
 
--- lab_9_10/neural.py
+++ lab_9_10/neural.py
@@ -1,6 +1,5 @@
-import random
-import sys
 import numpy as np
+import random
 
 class CrossEntropyCost(object):
     @staticmethod
@@ -21,7 +20,7 @@ class Network(object):
 
     def __init__(self, sizes: list, cost=CrossEntropyCost):
         """
-        The list sizes contains the number of neurons in the respective
+        The list sizes contains the number of neurons in the resigmoid_derivativeective
         layers of the network.  
         """
         self.num_layers = len(sizes)
@@ -31,13 +30,67 @@ class Network(object):
                         for x, y in zip(self.sizes[:-1], self.sizes[1:])]
         self.cost=cost
 
-    def feedforward(self, a : list) -> list:
-        """Return the output of the network if for a as input."""
+    def feedforward(self, a : np.ndarray) -> np.ndarray:
+        """Return the output of the network for a as input."""
         for b, w in zip(self.biases, self.weights):
             a = sigmoid(np.dot(w, a)+b)
         return a
 
-    # implement grad_descent here
+    def backprop(self, x, y):
+        """Return grad for single sample of bias and weight"""
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+
+        # Feedforward
+        activation = x
+        activations = [x]
+        z_list = [] # before activation
+
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w, activation) + b
+            z_list.append(z)
+            activation = sigmoid(z) # weight between <0,1>
+            activations.append(activation)
+
+        # Count error
+        delta = self.cost.delta(activations[-1], y)
+        nabla_b[-1] = delta
+        nabla_w[-1] = np.dot(delta, activations[-2].T)
+
+        # Backprop for hidden layers
+        for l in range(2, self.num_layers):
+            z = z_list[-l]
+            sigmoid_derivative = sigmoid_prime(z)
+            delta = np.dot(self.weights[-l + 1].T, delta) * sigmoid_derivative
+            nabla_b[-l] = delta
+            nabla_w[-l] = np.dot(delta, activations[-l - 1].T)
+
+        return nabla_b, nabla_w
+
+    def gradient_descent(self, training_data, max_steps, learning_rate):
+        """Gradient descent classic version"""
+        n = len(training_data)
+        for step in range(max_steps):
+            total_nabla_b = [np.zeros(b.shape) for b in self.biases]
+            total_nabla_w = [np.zeros(w.shape) for w in self.weights]
+
+            # Add all gradients
+            for x, y in training_data:
+                nabla_b, nabla_w = self.backprop(x, y)
+                total_nabla_b = [tb + nb for tb, nb in zip(total_nabla_b, nabla_b)]
+                total_nabla_w = [tw + nw for tw, nw in zip(total_nabla_w, nabla_w)]
+
+            self.weights = [w - (learning_rate / n) * dw
+                            for w, dw in zip(self.weights, total_nabla_w)]
+            self.biases = [b - (learning_rate / n) * db
+                           for b, db in zip(self.biases, total_nabla_b)]
+
+            print(f"step {step+1} finished")
+
+    def evaluate(self, test_data):
+        """Accuracy evaluation"""
+        results = [(np.argmax(self.feedforward(x)), np.argmax(y)) for x, y in test_data]
+        return sum(int(pred == label) for pred, label in results)
 
 def sigmoid(z):
     """The sigmoid function."""
